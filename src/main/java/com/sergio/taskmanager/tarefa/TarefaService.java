b6f833e7a009bef781c7cd85143f54a0258f90b8
sergiomansoo <sergio.manso2007@gmail.com>
2026-06-10 11:14:08 -0300

    fix: fix task-createById method

--- src/main/java/com/sergio/taskmanager/tarefa/TarefaService.java
+++ src/main/java/com/sergio/taskmanager/tarefa/TarefaService.java
@@ -83,6 +83,7 @@ public class TarefaService {
                 .dataEntrega(dto.dataEntrega())
                 .usuario(donoDaTarefa)
                 .build();
+        tarefaRepository.save(tarefa);
 
     }
 
